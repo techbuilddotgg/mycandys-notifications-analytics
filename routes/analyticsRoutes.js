eecc63780cbe3d512318d45a089ce905a13efdec
gregorsulcer <workcontroller0@gmail.com>
2023-12-21 22:59:42 +0100

    chore: added swagger

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -3,9 +3,67 @@ import { analyticsController } from "../controllers/analyticsController.js";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /analytics/getLastCalledEndpoint:
+ *   get:
+ *     summary: Get the latest accessed endpoint.
+ *     responses:
+ *       200:
+ *         description: Successful response with the latest accessed endpoint.
+ *       500:
+ *         description: Internal Server Error.
+ */
 router.get('/lastCalledEndpoint', analyticsController.getLastCalledEndpoint);
+/**
+ * @swagger
+ * /analytics/mostCalledEndpoint:
+ *   get:
+ *     summary: Get the most called endpoint.
+ *     responses:
+ *       200:
+ *         description: Successful response with the most called endpoint.
+ *       500:
+ *         description: Internal Server Error.
+ */
 router.get('/mostCalledEndpoint', analyticsController.mostCalledEndpoint);
+/**
+ * @swagger
+ * /analytics/numberOfCallsPerEndpoint:
+ *   get:
+ *     summary: Get count of calls for each endpoint.
+ *     responses:
+ *       200:
+ *         description: Successful response with the count of calls for each endpoint.
+ *       500:
+ *         description: Internal Server Error.
+ */
 router.get('/numberOfCallsPerEndpoint', analyticsController.numberOfCallsPerEndpoint);
+/**
+ * @swagger
+ * /stats:
+ *   post:
+ *     summary: Update statistics based on a remote service call.
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: calledService
+ *         required: true
+ *         description: The endpoint called by the remote service.
+ *         schema:
+ *           type: object
+ *           properties:
+ *             calledService:
+ *               type: string
+ *     responses:
+ *       200:
+ *         description: Successful update. Returns updated statistics.
+ *       500:
+ *         description: Internal Server Error.
+ */
 router.post('/updateData', analyticsController.updateData);
 
 export default router;
--- server.js
+++ server.js
@@ -3,12 +3,38 @@ import 'dotenv/config'
 import cors from 'cors';
 import { conn } from "./db/conn.js";
 import analyticsRouter from "./routes/analyticsRoutes.js";
-import { ApiCallModel } from "./models/apiCall.model.js";
-
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 const port = 3000 || process.env.PORT;
 conn();
+
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Notifications analytics API',
+            version: '1.0.0',
+            description: 'API documentation',
+        },
+        externalDocs: {
+            url: "/swagger.json"
+        },
+    },
+    apis: ['server.js', './routes/analyticsRoutes.js'],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get('/swagger.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+});
+app.get('/v3/api-docs', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+});
 
 app.use(cors());
 app.use(express.json());
@@ -24,6 +50,17 @@ app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
 
+/**
+ * @swagger
+ * /health:
+ *   get:
+ *     summary: Check if the service is running.
+ *     responses:
+ *       200:
+ *         description: Service is running.
+ *       500:
+ *         description: Service is not running.
+ */
 app.get('/health', async (req, res) => {
     res.status(200).json('Api is up and running');
 });
